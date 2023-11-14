9b1674bfebbecbc1bce7414f0f72f84a6ed04847
Raju <email.rajusarkar@gmail.com>
2023-11-14 18:05:13 +0530

    footer update

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,15 +45,7 @@ export default function RootLayout({
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
-							<Link
-								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
-							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
-							</Link>
+							<p className="font-bold">&#169; <span>2023</span> <span className="text-blue-600">Brew Haven</span></p>
 						</footer>
 					</div>
 				</Providers>
