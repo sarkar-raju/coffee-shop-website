296917bf0e9942f0907b994b1f66cc2e7bae0ffb
Raju <email.rajusarkar@gmail.com>
2023-11-14 18:00:44 +0530

    final-changes

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import {
   Table,
@@ -11,37 +11,85 @@ import {
 
 const Menu = () => {
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>NAME</TableColumn>
-        <TableColumn>ROLE</TableColumn>
-        <TableColumn>STATUS</TableColumn>
-      </TableHeader>
-      <TableBody>
-        <TableRow key="1">
-          <TableCell>Tony Reichert</TableCell>
-          <TableCell>CEO</TableCell>
-          <TableCell>Active
-            <button className="bg-blue-600">Buy</button>
-          </TableCell>
-        </TableRow>
-        <TableRow key="2">
-          <TableCell>Zoey Lang</TableCell>
-          <TableCell>Technical Lead</TableCell>
-          <TableCell>Paused</TableCell>
-        </TableRow>
-        <TableRow key="3">
-          <TableCell>Jane Fisher</TableCell>
-          <TableCell>Senior Developer</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="4">
-          <TableCell>William Howard</TableCell>
-          <TableCell>Community Manager</TableCell>
-          <TableCell>Vacation</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <>
+      <div className="text-center sm:mt-[80px] mt-[60px] mb-[40px]">
+        <h1 className="text-[60px] font-bold text-blue-600">Our Menu</h1>
+       
+      </div>
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>Price</TableColumn>
+          <TableColumn>Action</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key="1">
+            <TableCell>Espresso</TableCell>
+            <TableCell>$2.50</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="2">
+            <TableCell>Americano</TableCell>
+            <TableCell>$3.00</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="3">
+            <TableCell>Latte</TableCell>
+            <TableCell>$4.00</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="4">
+            <TableCell>Cappuccino</TableCell>
+            <TableCell>$4.50</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="5">
+            <TableCell>Mocha Madness</TableCell>
+            <TableCell>$5.00</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+          <TableRow key="6">
+            <TableCell>Caramel Dream Latte</TableCell>
+            <TableCell>$4.75</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+
+          <TableRow key="8">
+            <TableCell>Vanilla Bliss Cold Brew</TableCell>
+            <TableCell>$4.50</TableCell>
+            <TableCell>
+              <button className="bg-blue-600 rounded-full text-white font-bold  px-5 py-2">
+                Buy
+              </button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </>
   );
 };
 
